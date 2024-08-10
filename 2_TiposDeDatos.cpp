cf4ed17281c8439169348c3efa72973c8d374035
agusNazer <agustin.nazer@hotmail.com>
2024-08-09 23:53:32 -0300

    first commit

---
+++ 2_TiposDeDatos.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+
+//* 1Enteros                            Rangos
+short numero_mini = 10, z, p56 = -77;  //-32,564
+int valor, x = 21477, entero = 250;    // -2,123,345,125
+long grande1, grade2 = 2121231234324;  // -9,334,345,235,654,333
+
+//2.Reales (Decimales)                  //Rangos               Precision
+float valor_decimal, decimal = 10.23;   //(3.4 x 10) ^ -38  || 7 digitos
+double decimal_grande =880.3434434;     //(2.7 x 10) ^ -308 || 15 digitos
+long double p3 = 123.10111111;          //(3.4 x 10) ^ -4932|| 19 digitos
+
+//3.Caraceteres                
+
+char car = 's', car1 = 'Y';
+char ascii1 = 115, ascii2 = 89, caracter = 157;
+
+//4.Cadenas
+char cadena[] = "Oracion";
+char cadena2[100] = "Hola mundo!";
+
+//5.Booleanos
+bool b1, b2 = false, b3 = true;             //false = 0
+
+}
